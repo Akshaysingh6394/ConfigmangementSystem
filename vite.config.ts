a4d97cc5c299937cfe98e1a1578c7f9e7e49c841
Akshay Singh <singhakshay8081@gmail.com>
2025-07-17 02:55:08 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ConfigmanagementSystem/', // ⚠️ must match repo name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
